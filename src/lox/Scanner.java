32a605c056de55a30d3d7a39888bb9e5990dfe6c
0woy <qkrdbsdk1030@naver.com>
2024-02-21 23:35:07 +0900

    Scanner 문자, 숫자 리터럴 처리

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -43,16 +43,16 @@ public class Scanner {
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
             case '!':
-                addToken(match('='))?BANG_EQUAL:BANG;
+                addToken(match('=')?BANG_EQUAL:BANG);
                 break;
             case '=':
-                addToken(match('='))?EQUAL_EQUAL:EQUAL;
+                addToken(match('=')?EQUAL_EQUAL:EQUAL);
                 break;
             case '<':
-                addToken(match('='))?LESS_EQUAL:LESS;
+                addToken(match('=')?LESS_EQUAL:LESS);
                 break;
             case '>':
-                addToken(match('='))?GREATER_EQUAL:GREATER;
+                addToken(match('=')?GREATER_EQUAL:GREATER);
                 break;
             // 나눗셈 연산일 수도 있으나 주석일 수도 있음
             case '/':
@@ -64,17 +64,57 @@ public class Scanner {
                     addToken(SLASH);
                 }
                 break;
+
             case ' ':
             case '\t':
-            case '\r': break;
+            case '\r':
+                break;
             case '\n':
                 line++;
                 break;
+            case '"': string(); break;
+
             // 안 쓰는 문자가 소스파일에 있는 경우
-            default: 
-                Lox.error(line, "Unexpected character. ");
+            default:
+                // 숫자 리터럴
+                if (isDigit(c)) {
+                    number();
+                }
+                else {
+                    Lox.error(line, "Unexpected character. ");
+                }
                 break;
         }
+    }
+
+    // 숫자 리터럴 처리
+    private void number(){
+        while (isDigit(peek())) advance();  // 정수부 소비
+
+        // 소수부 소비 && . 바로 뒤에 숫자 존재 확인
+        if(peek()=='.' && isDigit(peekNext())){
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start,current)));
+    }
+    // 문자 리터럴 처리
+    private void string(){
+        while (peek()!='"' && !isAtEnd()){
+            if(peek()=='\n') line++;    // 여러 줄 문자열 지원
+            advance();
+        }
+        // 닫는 따옴표가 나오기 전에 문자가 소진된 경우 -> 에러
+        if(isAtEnd()){
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+        // 닫는 큰따옴표
+        advance();
+
+        // 앞 뒤 큰따옴표 제거
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
     }
 
     // 현재 문자가 찾으려는 문자와 매치되는 경우에만 소비
@@ -89,6 +129,16 @@ public class Scanner {
     private char peek(){
         if(isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext(){
+        if(current+1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+
+    // 숫자 여부 체크
+    private boolean isDigit(char c){
+        return c>='0' && c<='9';
     }
 
     // 문자를 모두 소비했는지 체크하는 헬퍼
